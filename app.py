6e8e22b811043989d20c706063c559d4efe852a0
kguralpdx <66285376+kguralpdx@users.noreply.github.com>
2020-08-30 18:14:22 -0700

    updated app.py and finally got precipitation section to work in notebook

--- app.py
+++ app.py
@@ -1,4 +1,26 @@
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///../Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to the tables
+Station = Base.classes.station
+Measurement = Base.classes.measurement
 #################################################
 # Flask Setup
 #################################################
@@ -14,20 +36,20 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"<a href='/api/v1.0/billingcountry'>billingcountry</a><br/>"
-        f"<a href='/api/v1.0/countrytotal'>countrytotal</a><br/>"
-        f"<a href='/api/v1.0/postcodes/USA'>postcodes/USA</a><br/>"
+        f"<a href='//api/v1.0/precipitation'>precipitation</a><br/>"
+        f"<a href='/api/v1.0/stations'>stations</a><br/>"
+        f"<a href='/api/v1.0/tobs/USC00519281'>tobs/USC00519281</a><br/>"
         f"<a href='/api/v1.0/countryitemtotals/USA'>countryitemtotals/USA</a><br/>"
         f"<a href='/api/v1.0/postcodeitemtotals/USA'>postcodeitemtotals/USA</a><br/>"
     )
 
-@app.route("/api/v1.0/billingcountry")
-def billingcountry():
+@app.route("/api/v1.0/precipitation")
+def precipitation():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
     # Query all countries in billing history
-    results = session.query(Invoices.BillingCountry).group_by(Invoices.BillingCountry).all()
+   # results = session.query(Measurement.BillingCountry).group_by(Measurement.BillingCountry).all()
 
 
     session.close()
@@ -38,33 +60,13 @@ def billingcountry():
     return jsonify(all_results)
 
 
-@app.route("/api/v1.0/countrytotal")
-def countrytotal():
+@app.route("/api/v1.0/stations")
+def stations():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    # Query all countries' invoice totals
-    results = session.query(Invoices.BillingCountry, func.sum(Invoices.Total)).group_by(Invoices.BillingCountry).order_by(func.sum(Invoices.Total).desc()).all()
-
-    session.close()
-
-    # Create a dictionary from the row data and append to a list of all_results
-    all_results = []
-    for item in results:
-        item_dict = {}
-        item_dict["country"] = item[0]
-        item_dict["total"] = float(item[1])
-        all_results.append(item_dict)
-
-    return jsonify(all_results)
-
-@app.route("/api/v1.0/postcodes/<value>")
-def postcodes(value):
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    # Query all countries in billing history
-    results = session.query(Invoices.BillingPostalCode).filter(Invoices.BillingCountry == value).group_by(Invoices.BillingPostalCode).all()
+    # Query all stations in Station
+    results = session.query(Station.station).group_by(Station.station).all()
 
     session.close()
 
@@ -72,6 +74,32 @@ def postcodes(value):
     all_results = list(np.ravel(results))
 
     return jsonify(all_results)
+
+@app.route("/api/v1.0/tobs/<value>")
+def tobs(value):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query all datas and temperatures of the most active station for the last year of data
+    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= "2019-08-18").filter(Measurement.station == value).order_by(Measurement.date).all()
+
+    session.close()
+
+    # Convert list of tuples into a normal list
+    all_results = list(np.ravel(results))
+
+    return jsonify(all_results)
+
+    ## Create a dictionary from the row data and append to a list of all_results
+    ##all_results = []
+    ##for item in results:
+    ##    item_dict = {}
+    ##    item_dict["date"] = item[0]
+    ##    item_dict["tobs"] = item[1]
+    ##    all_results.append(item_dict)
+
+    ##return jsonify(all_results)
+
 
 @app.route("/api/v1.0/countryitemtotals/<value>")
 def countryitemtotals(value):
