e8c0003c9549dfb0f24c9ad285a79463eb7bf21c
kguralpdx <66285376+kguralpdx@users.noreply.github.com>
2020-08-30 20:35:52 -0700

    got the first 3 api routes working in app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///../Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -36,11 +36,10 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"<a href='//api/v1.0/precipitation'>precipitation</a><br/>"
+        f"<a href='/api/v1.0/precipitation'>precipitation</a><br/>"
         f"<a href='/api/v1.0/stations'>stations</a><br/>"
         f"<a href='/api/v1.0/tobs/USC00519281'>tobs/USC00519281</a><br/>"
-        f"<a href='/api/v1.0/countryitemtotals/USA'>countryitemtotals/USA</a><br/>"
-        f"<a href='/api/v1.0/postcodeitemtotals/USA'>postcodeitemtotals/USA</a><br/>"
+#        f"<a href='/api/v1.0/countryitemtotals/USA'>countryitemtotals/USA</a><br/>"
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -49,13 +48,17 @@ def precipitation():
     session = Session(engine)
 
     # Query all countries in billing history
-   # results = session.query(Measurement.BillingCountry).group_by(Measurement.BillingCountry).all()
-
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= '2016-08-23').filter(Measurement.prcp != "None").order_by(Measurement.date).all()
 
     session.close()
 
-    # Convert list of tuples into normal list
-    all_results = list(np.ravel(results))
+    # Create a dictionary from the row data and append to a list of all_results
+    all_results = []
+    for item in results:
+        item_dict = {}
+        item_dict["date"] = item[0]
+        item_dict["prcp"] = item[1] #float(item[1])
+        all_results.append(item_dict)
 
     return jsonify(all_results)
 
@@ -81,7 +84,7 @@ def tobs(value):
     session = Session(engine)
 
     # Query all datas and temperatures of the most active station for the last year of data
-    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= "2019-08-18").filter(Measurement.station == value).order_by(Measurement.date).all()
+    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= "2016-08-18").filter(Measurement.station == value).order_by(Measurement.date).all()
 
     session.close()
 
@@ -101,34 +104,36 @@ def tobs(value):
     ##return jsonify(all_results)
 
 
-@app.route("/api/v1.0/countryitemtotals/<value>")
-def countryitemtotals(value):
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
+#@app.route("/api/v1.0/<start>/", defaults={"end": "2017-08-23"})
+#@app.route("/api/v1.0/<start>/<end>")
 
-    # Query all countries' invoice totals
-    results = session.query(func.sum(Items.UnitPrice * Items.Quantity)).filter(Invoices.InvoiceId == Items.InvoiceId).filter(Invoices.BillingCountry == value).scalar()
+#def countryitemtotals(value):
+#    # Create our session (link) from Python to the DB
+#    session = Session(engine)
 
-    session.close()
+#    # Query all countries' invoice totals
+#    results = session.query(func.sum(Items.UnitPrice * Items.Quantity)).filter(Invoices.InvoiceId == Items.InvoiceId).filter(Invoices.BillingCountry == value).scalar()
 
-    # Create a dictionary from the results
-    item_dict = {'country':value, 'total':float(results)}
+#    session.close()
 
-    return jsonify([item_dict])
+#   # Create a dictionary from the results
+#    item_dict = {'country':value, 'total':float(results)}
 
-@app.route("/api/v1.0/postcodeitemtotals/<value>")
-def postcodeitemtotals(value):
-    session = Session(engine)
-    results = session.query(Invoices.BillingPostalCode, func.sum(Items.UnitPrice * Items.Quantity)).filter(Invoices.InvoiceId == Items.InvoiceId).filter(Invoices.BillingCountry == value).group_by(Invoices.BillingPostalCode).order_by(func.sum(Items.UnitPrice * Items.Quantity).desc()).all()
-    session.close()
-    all_results = []
-    for item in results:
-        item_dict = {}
-        item_dict["postcode"] = item[0]
-        item_dict["total"] = float(item[1])
-        all_results.append(item_dict)
+#   return jsonify([item_dict])
 
-    return jsonify(all_results)
+#@app.route("/api/v1.0/postcodeitemtotals/<value>")
+#def postcodeitemtotals(value):
+#    session = Session(engine)
+#    results = session.query(Invoices.BillingPostalCode, func.sum(Items.UnitPrice * Items.Quantity)).filter(Invoices.InvoiceId == Items.InvoiceId).filter(Invoices.BillingCountry == value).group_by(Invoices.BillingPostalCode).order_by(func.sum(Items.UnitPrice * Items.Quantity).desc()).all()
+#    session.close()
+#    all_results = []
+#    for item in results:
+#        item_dict = {}
+#        item_dict["postcode"] = item[0]
+#        item_dict["total"] = float(item[1])
+#        all_results.append(item_dict)
+
+#    return jsonify(all_results)
 
 if __name__ == '__main__':
     app.run(debug=True)
